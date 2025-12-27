4e30c6ed9d19c13c922f9aa2f6abac2f3a105765
wjh <wjh2012@naver.com>
2025-12-27 18:56:55 +0900

    [backend]backend Init

---
+++ gg-shop-backend/src/main/java/ggomg/ggshopbackend/GgShopBackendApplication.java
@@ -0,0 +1,13 @@
+package ggomg.ggshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgShopBackendApplication.class, args);
+    }
+
+}
---
+++ gg-shop-backend/src/test/java/ggomg/ggshopbackend/GgShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ggomg.ggshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
